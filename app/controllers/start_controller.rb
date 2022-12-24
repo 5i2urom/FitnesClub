28d4d0ded26d92bd871986afc7ae390cd0ede505
RomchAss <gameathome2015@gmail.com>
2022-12-24 05:48:32 +0300

    again

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -8,29 +8,28 @@ class StartController < ApplicationController
     #   @rec = club.calendar_records.new()
     # end
 
-    ServiceAct.all.each do |service_act|
-      clubs = Club.joins(club_acts: :service_act).where("service_acts.activity = ?", service_act.activity)
-      len = clubs.length.clone
-      coaches = service_act.coaches
-      coaches.each do |coach|
-        #from = '26-12-2022'
-        #to = '31-12-2022'
-        dec = '-12-2022'
-        (26..31).each do |i|
-          beg = %w[10:30 12:30 14:30]
-          #finish = %w[12:30 14:30 16:30]
-          beg.each do |beg_each|
-            end_each = beg_each.clone
-            end_each[1] = (end_each[1].to_i + 2).to_s
-            rand_len=rand(len)
-            rec = coach.calendar_records.new(start_time: Date.parse(i.to_s+dec+beg_each),
-             end_time: Date.parse(i.to_s+dec+end_each))
-            rec.club = clubs[rand_len]
-            rec.save
-          end
-        end
-      end
-    end
-
+    # ServiceAct.all.each do |service_act|
+    #   clubs = Club.joins(club_acts: :service_act).where("service_acts.activity = ?", service_act.activity)
+    #   len = clubs.length.clone
+    #   coaches = service_act.coaches
+    #   coaches.each do |coach|
+    #     #from = '26-12-2022'
+    #     #to = '31-12-2022'
+    #     dec = '-12-2022'
+    #     (26..31).each do |i|
+    #       beg = %w[10:30 12:30 14:30]
+    #       #finish = %w[12:30 14:30 16:30]
+    #       beg.each do |beg_each|
+    #         end_each = beg_each.clone
+    #         end_each[1] = (end_each[1].to_i + 2).to_s
+    #         rand_len=rand(len)
+    #         rec = coach.calendar_records.new(start_time: Time.parse(i.to_s+dec+' '+beg_each),
+    #          end_time: Time.parse(i.to_s+dec+' '+end_each))
+    #         rec.club = clubs[rand_len]
+    #         rec.save
+    #       end
+    #     end
+    #   end
+    # end
   end
 end
