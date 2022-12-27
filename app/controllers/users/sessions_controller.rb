1b3a1ae96a91769c64fff3a8bec1ea06c6a57df5
RomchAss <gameathome2015@gmail.com>
2022-12-27 12:37:20 +0300

    i18-n + email_valid processes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,22 +6,30 @@ class Users::SessionsController < Devise::SessionsController
   # GET /resource/sign_in
   # def new
   #   super
-  # end
+  #   flash[:notice] = "Добро пожаловать, #{session["resource_name"]}!"
+    
+  #  end
 
   # POST /resource/sign_in
   def create
-    resource = warden.authenticate!(:scope => resource_name, :recall => "new")  
-    session["resource_email"] = resource.email
-    session["resource_name"] = resource.name
-    flash[:notice] = "Welcome, #{session["resource_name"]}!"
-    sign_in_and_redirect(resource_name, resource)
+    user = User.find_for_authentication(email: params[:user][:email])
+    if user.present? && user&.valid_password?(params[:user][:password]) then
+      resource = warden.authenticate!(:scope => resource_name, :recall => "new")  
+      session["resource_email"] = resource.email
+      session["resource_name"] = resource.name
+      flash[:notice] = "Добро пожаловать, #{session["resource_name"]}!"
+      sign_in_and_redirect(resource_name, resource)
+    else
+      render 'layouts/incorrect'      
+    end
   end
 
-  # DELETE /resource/sign_out
-  # def destroy  
-  #   flash[:notice] = "#{session["resource_name"]} signed out successfully.."  
-  #   sign_out_and_redirect(resource_name)  
-  # end 
+  #DELETE /resource/sign_out
+  def destroy
+    name = session["resource_name"]
+    super 
+    flash[:notice] = "До встречи, #{name}!"  
+  end 
  
 
   protected
